f331cb400f24b5ffbff5562338f1980a73a69f78
chetan <chetankharel7@gmail.com>
2021-10-16 11:24:17 +0545

    wip

--- tests/Feature/TodoListTest.php
+++ tests/Feature/TodoListTest.php
@@ -16,7 +16,7 @@ class TodoListTest extends TestCase
     public function setUp(): void
     {
         parent::setUp();
-        $this->list = TodoList::factory()->create(['name' => 'my todo list']);
+        $this->list = $this->createTodoList(['name' => 'my todo list']);
     }
 
 
@@ -55,6 +55,41 @@ class TodoListTest extends TestCase
         $this->assertEquals($list->name, $response['name']);
         $this->assertDatabaseHas('todo_lists', ['name' => $list->name]);
 
+    }
+
+    public function test_while_storing_todo_list_name_field_is_required()
+    {
+        $this->withExceptionHandling();
+
+        $this->postJson(route('todo-list.store'))
+            ->assertUnprocessable()
+            ->assertJsonValidationErrors(['name']);
+
+    }
+
+    public function test_delete_a_todo_list()
+    {
+        $this->deleteJson(route('todo-list.destroy', $this->list->id))->assertNoContent();
+
+        $this->assertDatabaseMissing('todo_lists', ['name' => $this->list->name]);
+    }
+
+    public function test_update_a_todo_list()
+    {
+
+        $this->patchJson(route('todo-list.update', $this->list->id), ['name' => 'updated name'])
+            ->assertOk();
+
+        $this->assertDatabaseHas('todo_lists', ['id' => $this->list->id, 'name' => 'updated name']);
+    }
+
+    public function test_while_updating_todo_list_name_field_is_required()
+    {
+        $this->withExceptionHandling();
+
+        $this->patchJson(route('todo-list.update', $this->list->id))
+            ->assertUnprocessable()
+            ->assertJsonValidationErrors(['name']);
     }
 
 
